2490f4fcbda6c6345ca28b154060fb3d07acc12f
Udayan Shiralkar <udayan75@hotmail.com>
2012-09-26 20:00:28 +0100

    hashes answers

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,15 @@
 # 1. create an array with three different string values
+letters = ["a","b","c","d"]
+puts letters
 
 # 2. add two new values to the Task #1 array in single expression
+letters.push("y","Z")
+puts letters
 
 # 3. convert your name to an array, and display it, in a single expression
-
+puts ["Udayan", "Shiralkar"]
 # 4. remove and display the last value in the Task # 1 array, and discuss
 # with the instructor why both values added in Task # 2 may have been removed
+last_letter = letters.pop
+puts last_letters
+
--- hashes.rb
+++ hashes.rb
@@ -1,9 +1,11 @@
 # 1. create a hash with variable name states comprised of three state names, including New York, and their abbreviations
- 
+ states = { ny: "New York", ca: "Cali", il: "Illinois"}
+
 # 2. create second hash with variable named request with url and ip as key names
-
+request = {url: "http://www.udayan.com" , ip: "10.20.30.40"}
 # 3. display the url in the request hash
-
+puts request[:url]
 # 4. display the abbreviation for New York
-
+puts states.key("New York")
 # 5. display whether the states hash contains the abbreviation NY
+puts states.has_key?(:ny)
